8eca4d555f8ef609bfcb39a19a70dd0e05f55bff
Marcos Lutterbach <mlutterb@gmail.com>
2022-08-19 09:32:11 +0200

    fixed the review modal button

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,6 +1,6 @@
 class Review < ApplicationRecord
   belongs_to :list
 
-  validates :comment,presence: true, length: { minimum: 6 }
+  validates :comment, presence: true, length: { minimum: 6 }
   validates :rating, numericality: { only_integer: true, greater_than_or_equal_to: 1, less_than_or_equal_to: 10 }
 end
